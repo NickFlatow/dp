92eade52743e17009abb6ca5f0fabfa42de1f95b
root <root@localhost.localdomain>
2021-06-24 15:47:40 -0500

    before changing hbthread

--- lib/thread.py
+++ lib/thread.py
@@ -1,19 +1,19 @@
 import threading
+import logging
 import time
 from lib import sasHandler
 
 threadLock = threading.Lock()
 
-class myThread (threading.Thread):
-   def __init__(self, threadID, name, counter):
+class lockedThread (threading.Thread):
+   def __init__(self, name):
       threading.Thread.__init__(self)
-      self.threadID = threadID
       self.name = name
-      self.counter = counter
    def run(self,cbsds,typeOfCalling):
-      print("Starting " + self.name)
+      logging.info("Starting thread" + self.name)
       # Get lock to synchronize threads
       threadLock.acquire()
       sasHandler.Handle_Request(cbsds,typeOfCalling)
       # Free lock to release next thread
       threadLock.release()
+   def hbThread()
