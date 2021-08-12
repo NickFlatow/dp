a9c09060ebee97feff21ef72d6b86976d8c04b5f
root <root@localhost.localdomain>
2021-08-12 16:31:59 -0500

    more cleanup

--- config/default.py
+++ config/default.py
@@ -1,11 +1,9 @@
 DEBUG = True
-EMAIL = "test@yahoo.com"
 PORT = "8089"
 HOST = "localhost"
 USER = "root"
 PASSWORD = "N3wPr1vateNw"
 # SAS = "https://192.168.4.222:5001/v1.2/"
 SAS = "https://test.sas.goog/v1.2/"
-# SAS = "https://192.168.4.223:5001/v1.2/"
 LOGFILE = "/tmp/dp.log"
  
